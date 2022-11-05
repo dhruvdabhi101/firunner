e890f9117a51530a1f84487eadac5d038c90126a
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2022-11-05 13:45:12 +0530

    bug fixes

--- dev/init.lua
+++ dev/init.lua
@@ -1,6 +1,6 @@
--- package.loaded['main'] = nil
--- package.loaded['main.runner'] = nil
--- package.loaded['dev'] = nil
+package.loaded['main'] = nil
+package.loaded['main.runner'] = nil
+package.loaded['dev'] = nil
 
 Firunner = require('main')
 vim.api.nvim_set_keymap('n',',v',':luafile dev/init.lua',{})
